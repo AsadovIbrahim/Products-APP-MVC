e1c226ef5323d73325a4ec2de5d32071fde24270
AsadovIbrahim <ibrahimasadov31@gmail.com>
2024-04-27 17:57:22 +0400

    commit

---
+++ Products-Project/Controllers/ProductController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Products_Project.Models;
+using System.Text.Json;
+using static Products_Project.Models.JsonHandler;
+
+namespace Products_Project.Controllers
+{
+    public class ProductController : Controller
+    {
+        
+        public List<Product> Products { get; set; } = new List<Product>();
+        public ProductController()
+        {
+            Products = ReadData<List<Product>>("products")!;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            if (Products.Exists(p=>p.Id==product.Id))
+            {
+                return View(product);
+
+            }
+            Products.Add(product);
+            WriteData(Products, "products");
+            return RedirectToAction("GetAllProducts");
+        }
+
+        [HttpPost]
+        public IActionResult GetProductById(int id)
+        {
+            var product = Products.FirstOrDefault(p => p.Id == id);
+            if (product != null) {
+                return View(product);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GetProductById()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GetProductByPrice(float price)
+        {
+            var product = Products.Where(p=>p.Price==price).ToList();
+            if (product != null)
+            {
+                return View(product);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetProductByPrice()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RemoveProduct(int id)
+        {
+            var product = Products.FirstOrDefault(p => p.Id == id);
+            if (product != null)
+            {
+                Products.Remove(product);
+                WriteData(Products, "products");
+            }
+            return RedirectToAction("GetAllProducts");
+        }
+
+        [HttpGet]
+        public IActionResult RemoveProduct()
+        {
+            return View();
+        }
+
+
+        public IActionResult GetAllProducts()
+        {
+            return View(Products);
+        }
+
+    }
+}
---
+++ Products-Project/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace Products_Project.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public float Price { get; set; }
+        public string ?Description { get; set; }
+
+        public Product(string name,float price,string description)
+        {
+            Name = name; 
+            Price = price;
+            Description = description;
+        }
+        public Product()
+        {
+        }
+        public override string ToString()
+        {
+            return $"Id:{Id}\nName:{Name}\nPrice:{Price}\nDescription:{Description}";
+        }
+    }
+}
